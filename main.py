a73e3719631af5044db74b5b4d7f54b1db52f531
Artem <av3411261@gmail.com>
2025-03-25 14:14:11 +0200

    Added comments to discord_bot.py and main.py files

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 from modules import bot_client, TOKEN
 
 def main():
-    # 
+    # Звертаємося до бота, та за допомогої run запускаємо його
     bot_client.run(TOKEN)
-# 
+# Виконуємо перевірку чи є це головним файлом для запуску проекту
 if __name__ == '__main__':
     main()
--- modules/discord_bot.py
+++ modules/discord_bot.py
@@ -6,33 +6,33 @@
 import discord
 import dotenv, os
 
-# 
+# Застосовуємо модуль dotenv, для того щоб зробити не видимою нашу змінну TOKEN
 dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-# 
+# Ставимо стандартні права для бота, та також даємо можливість боту читати повідомлення
 intents = discord.Intents.default()
 intents.message_content = True
 
-# 
+# Створюємо самого бота
 bot_client = discord.Client(intents= intents)
 
-# 
+# Викликаємо бота та звертаємося до його події
 @bot_client.event
-# 
+# Створюємо асинхронну функцію яка буде виводити повідомлення про те що бот запущено
 async def on_ready():
     print(f"Бот {bot_client.user} запущено")
 
-# 
+# Викликаємо бота та звертаємося до його події 
 @bot_client.event
-# 
+# Створюємо асинхронну функцію яка буде виводити повідомлення які надійшли від користувачів
 async def on_message(message):
-    # 
+    # Використовуємо конструкцію try except для того щоб вивести помилку якщо вона виникне
     try:
-        # 
+        # Робимо перевірку через умову чи повідомлення не від бота
         if message.author != bot_client.user:
             content = message.content
-            # 
+            # Виводимо повідомлення яке надійшло від користувача
             await message.channel.send(content)
     except:
         print("error")
